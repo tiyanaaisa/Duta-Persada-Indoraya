bcfd9b2ff856246e11a0bc5ab86f00093341415d
Tya Nasyah <tya@example.com>
2025-07-24 19:39:55 +0700

    fix: postcss config for Vercel build

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './style.css'
+import './input.css';
 
 console.log('Website Company Profile Active');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",              // index.html di root
+    "./src/**/*.{js,ts,jsx,tsx}" // semua file JS/TS di folder src
+  ],
   theme: {
-    extend: {},
+    extend: {}, // kamu bisa custom nanti di sini
   },
-  plugins: [],
+  plugins: [], // tambahkan plugin Tailwind di sini jika dibutuhkan
 }
